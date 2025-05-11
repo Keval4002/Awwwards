551bb3bcb6c95f01253de6f0fc95ac117b7d7646
Keval4002 <88760664+Keval4002@users.noreply.github.com>
2025-05-12 03:18:23 +0530

    Initialize React project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500">Welcome to awwwards</h1>
+    </main>
+  )
+}
+
+export default App
