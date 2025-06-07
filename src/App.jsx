97912326a69fd39cb02445a01ca4e1745f9f7b28
Keval Ambani <ambanikeval2@gmail.com>
2025-06-07 12:24:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Navbar />
-      <Hero />
+{/*       <Hero /> */}
       <About/>
       <Features/>
       <Story/>
